be855900c380a5f312bdb7cb2049b2598bec2f7e
Kowsika P <kowsika.p@zohocorp.com>
2019-05-06 11:06:48 +0530

    update

--- app/controllers/library/authors.js
+++ app/controllers/library/authors.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+	actions: {
+    filterByAuthorName(param) {
+      if (param !=='') {
+		return this.store.query('author', {filter:{ authorname:param}}).then((results) => {
+		return { query: param, results: results };
+		});
+   } else {
+		return this.store.findAll('author').then((results) => {
+		return { query: param, results: results };
+		});
+      }
+    }	
+  }
 });
--- app/controllers/library/books.js
+++ app/controllers/library/books.js
@@ -1,13 +1,18 @@
+
 import Controller from '@ember/controller';
 
 export default Controller.extend({
 	actions: {
     filterByBookName(param) {
-      if (param !== '') {
-        return this.store.query('book', { title: param });
-      } else {
-        return this.store.findAll('book');
+      if (param !=='') {
+		return this.store.query('book', {filter:{ title:param}}).then((results) => {
+		return { query: param, results: results };
+		});
+   } else {
+		return this.store.findAll('book').then((results) => {
+		return { query: param, results: results };
+		});
       }
-    }
+    }	
   }
 });
