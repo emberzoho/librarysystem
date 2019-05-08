74be600d9cae11b0bf215e057208b5d3eb48fa49
Harini.s <harini.sk@zohocorp.com>
2019-05-08 10:41:40 +0530

    update

---
+++ app/components/filter.js
@@ -0,0 +1,27 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    value: '',
+
+  init() {
+    this._super(...arguments);
+      this.filter('').then((allResults) => {
+      this.set('results', allResults);
+    });
+   
+  },
+
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.value;
+      let filterAction = this.filter;
+      filterAction(filterInputValue).then((filterResults) => {
+        //if (filterResults.query === arg) {
+          this.set('results', filterResults);//.results);
+        //}
+      });
+      //*/
+    }
+  }	
+});
