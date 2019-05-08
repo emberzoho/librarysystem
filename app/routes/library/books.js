8b75503ffa349572cd8ed92b2a6f6f7e799c5197
kowsika98 <49679181+kowsika98@users.noreply.github.com>
2019-05-08 14:41:48 +0530

    Update books.js

--- app/routes/library/books.js
+++ app/routes/library/books.js
@@ -1,14 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	beforeModel(){
-	//this.transitionTo('login');
-		var user = firebase.auth().currentUser;
-		if(!user){
-			this.transitionTo('login');
-		}
-	},
 	model() {
-		return this.store.findRecord('user', firebase.auth().currentUser.uid);  // => GET /blog-posts/1
+		return this.store.findRecord('user', firebase.auth().currentUser.uid);
   	}
 });
