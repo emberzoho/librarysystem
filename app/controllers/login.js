8157a5c8faefd8071f97418e0ad8c81953f73a11
Harini.S <harini.sk@zohocorp.com>
2019-05-09 12:52:24 +0530

    update

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,36 +3,32 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 		actions:{
 	sendPasswordReset() {
-      var email = document.getElementById('email').value;
-      // [START sendpasswordemail]
+      var email = this.get('email');
       firebase.auth().sendPasswordResetEmail(email).then(function() {
-        // Password Reset Email Sent!
-        // [START_EXCLUDE]
         alert('Password Reset Email Sent!');
-        // [END_EXCLUDE]
       }).catch(function(error) {
-        // Handle Errors here.
+        
         var errorCode = error.code;
         var errorMessage = error.message;
-        // [START_EXCLUDE]
+        
         if (errorCode == 'auth/invalid-email') {
           alert(errorMessage);
         } else if (errorCode == 'auth/user-not-found') {
           alert(errorMessage);
         }
         console.log(error);
-        // [END_EXCLUDE]
+        
       });
-      // [END sendpasswordemail];
+      
     },
     toggleSignIn() {
       if (firebase.auth().currentUser) {
-        // [START signout]
+        
         firebase.auth().signOut();
-        // [END signout]
+        
       } else {
-        var email = document.getElementById('email1').value;
-        var password = document.getElementById('password1').value;
+        var email = this.get('email');
+        var password = this.get('password');
         if (email.length < 4) {
           alert('Please enter an email address.');
           return;
@@ -41,25 +37,30 @@ export default Controller.extend({
           alert('Please enter a password.');
           return;
         }
-        // Sign in with email and pass.
-        // [START authwithemail]
-        firebase.auth().signInWithEmailAndPassword(email, password).then(()=>{this.transitionToRoute('library.books');}).catch(function(error) {
-          // Handle Errors here.
+        
+        firebase.auth().signInWithEmailAndPassword(email, password).then(()=>{this.transitionToRoute('library.books');
+            this.setProperties({
+              email:'',
+              password:''
+            });
+
+        }).catch(function(error) {
+          
           var errorCode = error.code;
           var errorMessage = error.message;
-          // [START_EXCLUDE]
+          
           if (errorCode === 'auth/wrong-password') {
             alert('Wrong password.');
           } else {
             alert(errorMessage);
           }
           console.log(error);
-          //document.getElementById('quickstart-sign-in').disabled = false;
-          // [END_EXCLUDE]
+          
         });
-        // [END authwithemail]
+        
+        
       }
-      //document.getElementById('quickstart-sign-in').disabled = true;
+      
     }
 },
 });
